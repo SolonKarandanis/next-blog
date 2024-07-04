f311816f7df761b09b75057d1fc42786081cc925
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-07-04 20:30:16 +0300

    Posts page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,9 +1,27 @@
+import Link from 'next/link';
 import React from 'react'
 
-const PostsPage = () => {
-  return (
-    <div>page</div>
-  )
+const PostsPage = async () => {
+    const response= await fetch('https://dummyjson.com/posts');
+    const data = await response.json();
+    return (
+        <main className='text-center pt-16 px-5'>
+            <h1 className="text-4xl md:text-5xl font-bold mb-5">
+               All posts
+            </h1>
+            <ul>
+                {
+                    data.posts.map((post:any) =>(
+                        <li key={post.id} className='mb-3'>
+                            <Link href={`/posts/${post.id}`}>
+                                {post.title}
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
+        </main>
+    )
 }
 
 export default PostsPage
