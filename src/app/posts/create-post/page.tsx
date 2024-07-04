2e7455db7298b2663fde5a211cacdf17aac339ab
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-07-04 20:56:58 +0300

    Create post page

---
+++ src/app/posts/create-post/page.tsx
@@ -0,0 +1,10 @@
+export default async function Page() {
+    return (
+        <main className="text-center pt-16">
+            <h1 className="text-4xl md:text-5xl font-bold mb-5">
+                Create post
+            </h1>
+            {/* <Form /> */}
+        </main>
+    );
+}
