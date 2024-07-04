2deab2c3852ce82d884f1ee684c1af7de58d764e
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-07-04 18:53:31 +0300

    Small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 
 export default function Home() {
   return (
-    <main>
-   
+    <main className="text-center pt-32 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5 ">
+        Welcome to my blog
+      </h1>
+      <p className="max-w-[750px] mx-auto leading-8">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Eligendi possimus doloremque reprehenderit laborum dolorem delectus quis non, 
+        aliquid in harum magnam pariatur aliquam alias, aspernatur sunt 
+        architecto voluptatem minima numquam!
+      </p>
     </main>
   );
 }
