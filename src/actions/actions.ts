143445825d49c9f0b49190da868fd5852030cced
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-07-06 10:37:47 +0300

    Created

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,11 +1,18 @@
 "use server"
 
 import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
+import prisma from "@/lib/db";
 
 export async function createPost(formData: FormData){
     const title = formData.get("title") as string;
     const body = formData.get("body") as string;
+
+    await prisma.post.create({
+        data: {
+          title,
+          body,
+        },
+    });
 
     revalidatePath("/posts");
 }
--- src/app/components/posts-list.tsx
+++ src/app/components/posts-list.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
-// import prisma from "@/lib/db";
+import prisma from "@/lib/db";
 
 export default async function PostsList(){
-    const response= await fetch('https://dummyjson.com/posts');
-    const data = await response.json();
-    // const posts = await prisma.post.findMany();
+  const posts = await prisma.post.findMany();
+    
     return (
         <ul>
-          {data.posts.map((post:any) => (
+          {posts.map((post:any) => (
             <li key={post.id} className="mb-3">
               <Link href={`/posts/${post.id}`}>{post.title}</Link>
             </li>
