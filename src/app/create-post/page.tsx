4aa69f2a3060c8d37b7c3ace6517598027c5543f
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-07-06 08:57:18 +0300

    Create Post functionality

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -1,10 +1,12 @@
+import Form from "../components/form";
+
 export default async function Page() {
     return (
         <main className="text-center pt-16">
             <h1 className="text-4xl md:text-5xl font-bold mb-5">
                 Create post
             </h1>
-            {/* <Form /> */}
+            <Form />
         </main>
     );
 }
