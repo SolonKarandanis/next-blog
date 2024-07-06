0fa1c0c01878a2f4f8307ed105e40251d759f351
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-07-06 09:26:47 +0300

    Initialized prisma

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -3,6 +3,9 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-export async function createPost(){
+export async function createPost(formData: FormData){
+    const title = formData.get("title") as string;
+    const body = formData.get("body") as string;
 
+    revalidatePath("/posts");
 }
